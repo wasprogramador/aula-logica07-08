61704e84bd88ac2afe80f07604ae53685cbe9698
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-08-09 17:23:00 -0300

    exercicio de estoque

--- aula05.py
+++ aula05.py
@@ -1,1 +1,10 @@
-print("estamos na branch test")
+estoque = 50
+quantidade_desejada = 30
+
+if estoque . 0
+    if quantidade_desejada <= estoque:
+        print("Produto disponivel para compra")
+    else:
+        print("wuantidade desejada não disponivel.")
+else:
+    print("Produto fora de estoque")
