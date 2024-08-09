a7804d7a643de864820117456670bad57b024060
Wasprogramador <00001114060525sp@al.educacao.sp.gov.br>
2024-08-09 17:29:02 -0300

    nova versão do exercicio

---
+++ aula05.py
@@ -0,0 +1,11 @@
+print("exercicio")
+estoque = 50
+quantidade_desejada = 30
+
+if estoque . 0
+    if quantidade_desejada <= estoque:
+        print("Produto disponivel para compra")
+    else:
+        print("wuantidade desejada não disponivel.")
+else:
+    print("Produto fora de estoque")
